5f606baf65f3a95492d95abf4f5a56dbfe011f4b
faridcloud <72005523+faridcloud@users.noreply.github.com>
2021-01-18 13:47:42 +0700

    Add files via upload

--- BukuAlamat/src/bukualamat/BukuAlamat.java
+++ BukuAlamat/src/bukualamat/BukuAlamat.java
@@ -5,15 +5,19 @@
  */
 package bukualamat;
 
+import bukualamat.model.DataPersonal;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
 
 /**
  *
@@ -24,6 +28,21 @@ public class BukuAlamat extends Application {
     //variabel kelas
     private Stage stageAplikasi;
     private BorderPane rootAplikasi;
+    //listdataperson bertipe ObservableList, yang berisi data dari model DataPersonal
+    private ObservableList<DataPersonal> dataperson = FXCollections.observableArrayList();
+    
+    //Konstruksi untuk inisialisasi data sebagai bahan tes
+    public BukuAlamat(){
+        dataperson.add(new DataPersonal("Budi","Santoso"));
+        dataperson.add(new DataPersonal("Ahmad","Dahlan"));
+        dataperson.add(new DataPersonal("Hasyim","Asy'ari"));
+        dataperson.add(new DataPersonal("Anies","Baswedan"));
+        dataperson.add(new DataPersonal("Ridwan","Kamil"));
+    }
+    //fungsi untuk membaca data list
+    public ObservableList<DataPersonal> getDataPersonal(){
+        return dataperson;
+    }
     
     @Override
     public void start(Stage primaryStage){
@@ -31,7 +50,7 @@ public class BukuAlamat extends Application {
         this.stageAplikasi.setTitle("Buku Alamat");
         
         initGuiKerangka();//fungsi inisialisasi node dari GuiKerangka.fxml
-        //initViewBukuAlamat();//fungsi inisialisasi node dari viewBukuAlamat.fxml
+        initViewBukuAlamat();//fungsi inisialisasi node dari viewBukuAlamat.fxml
     }
 
     /**
@@ -42,17 +61,27 @@ public class BukuAlamat extends Application {
     }
 
     private void initGuiKerangka() {
-        try {
-            FMXLLoader loader = new FMXLLoader();
+        try{
+            FXMLLoader loader = new FXMLLoader();
             loader.setLocation(BukuAlamat.class.getResource("view/GuiKerangka.fxml"));
             rootAplikasi = (BorderPane)loader.load();
             
             Scene sceneAplikasi = new Scene(rootAplikasi);
             stageAplikasi.setScene(sceneAplikasi);
             stageAplikasi.show();
-        } catch (IOException ex){
-            logger.getLogger(BukuAlamat.class.getName()).log(level.SEVERE,null,ex);
+        }catch (IOException ex){
+            Logger.getLogger(BukuAlamat.class.getName()).log(Level.SEVERE,null,ex);
         }
     }
     
+    private void initViewBukuAlamat(){
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(BukuAlamat.class.getResource("view/viewBukuAlamat.fxml"));
+            AnchorPane guiIsi = (AnchorPane)loader.load();
+            rootAplikasi.setCenter(guiIsi);
+        }catch(IOException ex){
+            Logger.getLogger(BukuAlamat.class.getName()).log(Level.SEVERE,null,ex);
+        }
+    }
 }
